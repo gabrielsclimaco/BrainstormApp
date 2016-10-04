3bdb0cd17eae929f9ebec8a1cdebd574eff1104b
climaco02 <gabrielsclimaco@gmail.com>
2016-10-04 12:09:01 -0300

    Setting up parse

--- app/src/main/java/com/reforcointeligente/brainstormapp/Controller/Parse/ParseInitializer.java
+++ app/src/main/java/com/reforcointeligente/brainstormapp/Controller/Parse/ParseInitializer.java
@@ -1,4 +1,39 @@
 package com.reforcointeligente.brainstormapp.Controller.Parse;
 
-public class ParseInitializer {
+import com.parse.Parse;
+import com.parse.ParseObject;
+import com.reforcointeligente.brainstormapp.Model.Student;
+
+public class ParseInitializer extends android.app.Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        initializeParse();
+    }
+
+    private boolean initializeParse(){
+        try {
+            Parse.initialize(new Parse.Configuration.Builder(this)
+                    .applicationId("brainstorm-freelancer")
+                    .server("http://brainstorm-freelancer.herokuapp.com")
+//                    .clientKey("kijasijijasiasjsiajalllkaosiajhsis")
+                    .build()
+            );
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+//    public boolean registerParserSubClasses() {
+//        try {
+//            ParseObject.registerSubclass(Student.class);
+//            return true;
+//        } catch (Exception e) {
+//            return false;
+//        }
+//    }
 }

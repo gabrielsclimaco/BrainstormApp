ac23b03f0844f959e88f2aa475bd501df3f22b09
climaco02 <gabrielsclimaco@gmail.com>
2016-10-22 17:42:14 -0200

    Setting up Lesson model

--- app/src/main/java/com/reforcointeligente/brainstormapp/Model/Lesson.java
+++ app/src/main/java/com/reforcointeligente/brainstormapp/Model/Lesson.java
@@ -12,25 +12,140 @@ public class Lesson extends ParseObject {
     //valor pago ao professor (valor da hora/aula * horas de aula),
     //lucro da aula (valor total da aula - valor pago ao professor)
 
-    private String getLessonDate() {
-        return getLessonDate();
+    public String getLessonDate() {
+        return getString(getLessonDateTitle());
     }
 
-    private void setLessonDate(Date lessonDate) {
-        put(getLessonDate(), lessonDate);
+    public void setLessonDate(String lessonDate) {
+        put(getLessonDateTitle(), lessonDate);
     }
 
-    private String getLessonTime() {
-        return getLessonTime();
+    public String getLessonTime() {
+        return getString(getLessonTimeTitle());
     }
 
-    private void setLessonTime(Time lessonTime) {
-        put(getLessonTime(), lessonTime);
+    public void setLessonTime(String lessonTime) {
+        put(getLessonTimeTitle(), lessonTime);
+    }
+
+    public String getLessonTeacher() {
+        return getString(getLessonTeacherTitle());
+    }
+
+    public void setLessonTeacher(String teacherName) {
+        put(getLessonTeacherTitle(), teacherName);
+    }
+
+    public String getLessonStudent() {
+        return getString(getLessonStudentTitle());
+    }
+
+    public void setLessonStudent(String studentName) {
+        put(getLessonStudentTitle(), studentName);
+    }
+
+    public String getLessonSubject() {
+        return getString(getLessonSubjectTitle());
+    }
+
+    public void setLessonSubject(String subject){
+        put(getLessonSubjectTitle(), subject);
+    }
+
+    public String getLessonPlace() {
+        return getString(getLessonPlaceTitle());
+    }
+
+    public void setLessonPlace(String place) {
+        put(getLessonPlaceTitle(), place);
+    }
+
+    public Double getLessonPricePerHour() {
+        return getDouble(getLessonPricePerHourTitle());
+    }
+
+    public void setLessonPricePerHour(String pricePerHour) {
+        put(getLessonPricePerHourTitle(), pricePerHour);
+    }
+
+    public Double getLessonDuration() {
+        return getDouble(getLessonDurationTitle());
+    }
+
+    public void setLessonDuration(Double Duration){
+        put(getLessonDurationTitle(), Duration);
+    }
+
+    public Double getLessonTotalValue() {
+        return getDouble(getLessonTotalValueTitle());
+    }
+
+    public void getLessonTotalValues(Double totalValue){
+        put(getLessonTotalValueTitle(), totalValue);
+    }
+
+    public Double getLessonTeacherValue() {
+        return getDouble(getLessonTeacherValueTitle());
+    }
+
+    public void setLessonTeacherValue(Double teacherValue) {
+        put(getLessonTeacherValueTitle(), teacherValue);
+    }
+
+    public Double getLessonProfit() {
+        return getDouble(getLessonProfitTitle());
+    }
+
+    public void setLessonProfit(Double profit) {
+        put(getLessonProfitTitle(), profit);
     }
 
     public static ParseQuery<Lesson> getQuery() {
         return ParseQuery.getQuery(Lesson.class);
     }
 
+    private static String getLessonDateTitle () {
+        return "data_aula";
+    }
+
+    private static String getLessonTimeTitle () {
+        return "horario_aula";
+    }
+
+    private static String getLessonTeacherTitle () {
+        return "professor_aula";
+    }
+
+    private static String getLessonStudentTitle () {
+        return "aluno_aula";
+    }
+
+    private static String getLessonSubjectTitle () {
+        return "disciplina_aula";
+    }
+
+    private static String getLessonPlaceTitle () {
+        return "local_aula";
+    }
+
+    private static String getLessonPricePerHourTitle () {
+        return "valor_hora_aula";
+    }
+
+    private static String getLessonDurationTitle () {
+        return "duracao_aula";
+    }
+
+    private static String getLessonTotalValueTitle () {
+        return "valor_total_aula";
+    }
+
+    private static String getLessonTeacherValueTitle () {
+        return "valor_professor_aula";
+    }
+
+    private static String getLessonProfitTitle () {
+        return "lucro_aula";
+    }
 
 }

46e9d7eea31e5858bed480516e439ea6e84073d5
climaco02 <climaco.cafe@gmail.com>
2016-10-22 18:42:42 -0200

    Setting up Teacher model

--- app/src/main/java/com/reforcointeligente/brainstormapp/Model/Teacher.java
+++ app/src/main/java/com/reforcointeligente/brainstormapp/Model/Teacher.java
@@ -11,68 +11,127 @@ public class Teacher extends ParseObject {
 
     //Return teacher's name
     private String getTeacherName() {
-        return getString(getTeacherName());
+        return getString(getTeacherNameTitle());
     }
 
     //Set teacher's name
     private void setTeacherName(String teacherName) {
-        put(getTeacherName(), teacherName);
+        put(getTeacherNameTitle(), teacherName);
     }
 
     //Return teacher's address
     private String getTeacherAddress() {
-        return getString(getTeacherAddress());
+        return getString(getTeacherAddressTitle());
     }
 
     //Set teacher's address
     private void setTeacherAddress(String teacherAddress) {
-        put(getTeacherAddress(), teacherAddress);
-    }
-
-    //Return neighborhood
-    private String getTeacherNeighborhood() {
-        return getString(getTeacherNeighborhood());
-    }
-
-    //Set neighborhood
-    private void setTeacherNeighborhood(String teacherNeighborhood) {
-        put(getTeacherNeighborhood(), teacherNeighborhood);
+        put(getTeacherAddressTitle(), teacherAddress);
     }
 
     //Return city
     private String getTeacherCity() {
-        return getString(getTeacherCity());
+        return getString(getTeacherCityTitle());
     }
 
     //Set city
     private void setTeacherCity(String teacherCity) {
-        put(getTeacherCity(), teacherCity);
+        put(getTeacherCityTitle(), teacherCity);
     }
 
     //Return teacher's residencial phone
     public String getTeacherPhone(){
-        return getString(getTeacherPhone());
+        return getString(getTeacherPhoneTitle());
     }
 
     //Set teacher's residencial phone
     public void setTeacherPhone(String teacherPhone){
-        put(getTeacherPhone(), teacherPhone);
+        put(getTeacherPhoneTitle(), teacherPhone);
     }
 
     //Return teacher's cellphone
     public String getTeacherCellphone(){
-        return getString(getTeacherCellphone());
+        return getString(getTeacherCellphoneTitle());
     }
 
     //Set teacher's cellphone
     public void setTeacherCellphone(String teacherCellphone){
-        put(getTeacherCellphone(), teacherCellphone);
+        put(getTeacherCellphoneTitle(), teacherCellphone);
+    }
+
+    public String getTeacherCourse() {
+        return getString(getTeacherCourseTitle());
+    }
+
+    public void setTeacherCourse(String course) {
+        put(getTeacherCourseTitle(), course);
+    }
+
+    public Boolean getTeacherCar() {
+        return getBoolean(getTeacherCarTitle());
+    }
+
+    public void setTeacherCar(Boolean car){
+        put(getTeacherCarTitle(), car);
+    }
+
+    public Double getTeacherPricePerHour() {
+        return getDouble(getTeacherPricePerHourTitle());
+    }
+
+    public void setTeacherPricePerHour(Double pricePerHour) {
+        put(getTeacherPricePerHourTitle(), pricePerHour);
+    }
+
+    // NOT FINISHED
+    public String getTeacherSubject() {
+        return getString(getTeacherSubjectTitle());
+    }
+
+    public void setTeacherSubject(String subject) {
+        put(getTeacherSubjectTitle(), subject);
     }
 
     public static ParseQuery<Teacher> getQuery() {
         return ParseQuery.getQuery(Teacher.class);
     }
 
+
+    private static String getTeacherNameTitle() {
+        return "nome_professor";
+    }
+
+    private static String getTeacherAddressTitle() {
+        return "endereco_professor";
+    }
+
+    private static String getTeacherCityTitle() {
+        return "cidade_professor";
+    }
+
+    private static String getTeacherPhoneTitle() {
+        return "telefone_professor";
+    }
+
+    private static String getTeacherCellphoneTitle(){
+        return "celular_professor";
+    }
+
+    private static String getTeacherCourseTitle() {
+        return "curso_professor";
+    }
+
+    private static String getTeacherSubjectTitle() {
+        return "materias_professor";
+    }
+
+    private static String getTeacherCarTitle() {
+        return "possui_carro_professor";
+    }
+
+    private static String getTeacherPricePerHourTitle() {
+        return "preco_hora_professor";
+    }
 }
 
 

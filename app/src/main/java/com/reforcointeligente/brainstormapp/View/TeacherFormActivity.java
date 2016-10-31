0352ba5952553ca1c1df7ff749cc32d5ce3e6440
climaco02 <gabrielsclimaco@gmail.com>
2016-10-31 14:44:56 -0200

    Setting up Teacher city spinner

--- app/src/main/java/com/reforcointeligente/brainstormapp/View/TeacherFormActivity.java
+++ app/src/main/java/com/reforcointeligente/brainstormapp/View/TeacherFormActivity.java
@@ -2,14 +2,28 @@ package com.reforcointeligente.brainstormapp.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.reforcointeligente.brainstormapp.R;
 
 public class TeacherFormActivity extends AppCompatActivity {
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_form);
+
+        setUpSpinner();
+    }
+
+    private void setUpSpinner() {
+        spinner = (Spinner) findViewById(R.id.spinnerTeacherCity);
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this,
+                R.array.list_df_cities, android.R.layout.simple_spinner_dropdown_item);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(arrayAdapter);
     }
 }

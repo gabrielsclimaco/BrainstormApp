2094371f28dd7ba68f31883ca99da375a5826eba
Cecilia <ceciliabessa@hotmail.com>
2016-11-21 11:24:23 -0200

    Create and set student information

--- app/src/main/java/com/reforcointeligente/brainstormapp/View/StudentFormActivity.java
+++ app/src/main/java/com/reforcointeligente/brainstormapp/View/StudentFormActivity.java
@@ -5,19 +5,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.reforcointeligente.brainstormapp.Model.Student;
 import com.reforcointeligente.brainstormapp.R;
 
 public class StudentFormActivity extends AppCompatActivity {
     Spinner spinner;
+    Button confirmStudentButton;
+    Button cancelStudentButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_form);
-
         setUpSpinner();
+
+        cancelStudentButton = (Button) findViewById(R.id.buttonCancelStudent);
+        cancelStudentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
     private void setUpSpinner() {
@@ -27,6 +39,40 @@ public class StudentFormActivity extends AppCompatActivity {
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinner.setAdapter(arrayAdapter);
+    }
+
+    public void onTeacherCreated() {
+        Student student = new Student();
+
+        EditText studentNameEditText = (EditText) findViewById(R.id.editTextStudentName);
+        EditText studentAgeEditText = (EditText) findViewById(R.id.editTextStudentAge);
+        EditText studentSchoolEditText = (EditText) findViewById(R.id.editTextStudentSchool);
+        EditText studentAddressEditText = (EditText) findViewById(R.id.editTextStudentAddress);
+        EditText studentParentNameEditText = (EditText) findViewById(R.id.editTextStudentParentName);
+        EditText studentParentCellphoneEditText = (EditText) findViewById(R.id.editTextStudentParentCellphone);
+        EditText studentParentPhoneEditText = (EditText) findViewById(R.id.editTextStudentParentPhone);
+        EditText studentParentEmailEditText = (EditText) findViewById(R.id.editTextStudentParentEmail);
+
+        String studentName = studentNameEditText.getText().toString();
+        String studentAge = studentAgeEditText.getText().toString();
+        String studentSchool = studentSchoolEditText.getText().toString();
+        String studentAddress = studentAddressEditText.getText().toString();
+        String studentCity = spinner.getSelectedItem().toString();
+        String studentParentName = studentParentNameEditText.getText().toString();
+        String studentParenteCellphone = studentParentCellphoneEditText.getText().toString();
+        String studentParentPhone = studentParentPhoneEditText.getText().toString();
+        String studentParentEmail = studentParentEmailEditText.getText().toString();
+
+        student.setStudentName(studentName);
+        student.setStudentAge(studentAge);
+        student.setStudentSchool(studentSchool);
+        student.setStudentAddress(studentAddress);
+        student.setStudentCity(studentCity);
+        student.setStudentParentName(studentParentName);
+        student.setStudentParentCellphone(studentParenteCellphone);
+        student.setStudentParentPhone(studentParentPhone);
+        student.setStudentParentEmail(studentParentEmail);
+
     }
 
 }

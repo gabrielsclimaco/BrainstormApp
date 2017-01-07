f2759f313a0d01d0f031607960c6ee89e4e208ea
Cecilia <ceciliabessa@hotmail.com>
2017-01-07 18:05:13 -0200

    creating lesson

--- app/src/main/java/com/reforcointeligente/brainstormapp/Controller/FirebaseUtils.java
+++ app/src/main/java/com/reforcointeligente/brainstormapp/Controller/FirebaseUtils.java
@@ -10,6 +10,8 @@ import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.reforcointeligente.brainstormapp.Model.Student;
+import com.reforcointeligente.brainstormapp.Model.Lesson;
+import com.reforcointeligente.brainstormapp.Model.Teacher;
 import com.reforcointeligente.brainstormapp.R;
 
 public class FirebaseUtils {
@@ -18,6 +20,7 @@ public class FirebaseUtils {
     public static void saveStudent(View view){
         String name = ((EditText) view.findViewById(R.id.editTextStudentName)).getText().toString();
         String school = ((EditText) view.findViewById(R.id.editTextStudentSchool)).getText().toString();
+        String schoolYear = ((EditText) view.findViewById(R.id.editTextStudentSchoolYear)).getText().toString();
         String address = ((EditText) view.findViewById(R.id.editTextStudentAddress)).getText().toString();
         String city = ((Spinner) view.findViewById(R.id.spinnerStudentCity)).getSelectedItem().toString();
         String nameParent = ((EditText) view.findViewById(R.id.editTextStudentParentName)).getText().toString();
@@ -31,7 +34,7 @@ public class FirebaseUtils {
             age = Integer.valueOf(((EditText) view.findViewById(R.id.editTextStudentAge)).getText().toString());
         }
 
-        Student studentToSave = new Student(name, age, address, city, "", school, nameParent,
+        Student studentToSave = new Student(name, age, address, city, schoolYear, school, nameParent,
                 phoneParent, cellphoneParent, emailParent);
 
         databaseReference.child("Students").push().setValue(studentToSave);
@@ -49,5 +52,85 @@ public class FirebaseUtils {
         };
 
         return adapter;
+    }
+
+    public static void saveLesson(View view){
+        String date = ((EditText) view.findViewById(R.id.editTextLessonDate)).getText().toString();
+        String time = ((EditText) view.findViewById(R.id.editTextLessonTime)).getText().toString();
+        //Teacher teacher = ((Spinner) view.findViewById(R.id.spinnerLessonTeacher)).getSelectedItem();
+        //Student student = ((Spinner) view.findViewById(R.id.spinnerLessonStudent)).getSelectedItem();
+        String subject = ((Spinner) view.findViewById(R.id.spinnerLessonSubject)).getSelectedItem()
+                .toString();
+        String place = ((EditText) view.findViewById(R.id.editTextLessonPlace)).getText()
+                .toString();
+
+        // assures that int is not a empty string
+        Double displacement = 0.0;
+        if (!((EditText) view.findViewById(R.id.editTextLessonDisplacement)).getText()
+                .toString().equals("")) {
+            displacement = Double.valueOf(((EditText)
+                    view.findViewById(R.id.editTextLessonDisplacement)).getText().toString());
+        }
+
+        // assures that int is not a empty string
+        Double valuePerHour = 0.0;
+        if (!((EditText) view.findViewById(R.id.editTextLessonValuePerHour)).getText()
+                .toString().equals("")) {
+            valuePerHour = Double.valueOf(((EditText)
+                    view.findViewById(R.id.editTextLessonValuePerHour)).getText().toString());
+        }
+
+        // assures that int is not a empty string
+        Double duration = 0.0;
+        if (!((EditText) view.findViewById(R.id.editTextLessonDuration)).getText()
+                .toString().equals("")) {
+            duration = Double.valueOf(((EditText)
+                    view.findViewById(R.id.editTextLessonDuration)).getText().toString());
+        }
+
+        Student student = new Student();
+        Teacher teacher = new Teacher();
+
+        Lesson lessonToSave = new Lesson(date, time, teacher, student, subject,
+                place, displacement, valuePerHour, duration);
+
+        databaseReference.child("Lessons").push().setValue(lessonToSave);
+
+    }
+
+    public static FirebaseListAdapter<Lesson> loadLessons(FragmentActivity fragmentActivity) {
+        FirebaseListAdapter<Lesson> adapter = new FirebaseListAdapter<Lesson>(fragmentActivity, Lesson.class,
+                android.R.layout.two_line_list_item,
+                databaseReference.child("Lessons")) {
+            @Override
+            protected void populateView(View view, Lesson lesson, int position) {
+                ((TextView) view.findViewById(android.R.id.text1)).setText(lesson.getLessonDate());
+            }
+        };
+
+        return adapter;
+    }
+
+    public static void saveTeacher(View view){
+//        private String teacherName;
+//        private String teacherAddress;
+//        private String teacherCity;
+//        private String teacherPhone;
+//        private String teacherCellphone;
+//        private String teacherCourse;
+//        private Boolean teacherCar;
+//        private Double pricePerHour;
+//        private String teacherEmail;
+//        private List<String> teacherSubjects;
+
+        String name = ((EditText) view.findViewById(R.id.editTextTeacherName)).getText().toString();
+        String address = ((EditText) view.findViewById(R.id.editTextTeacherAddress)).getText().toString();
+        String city = ((Spinner) view.findViewById(R.id.spinnerTeacherCity)).getSelectedItem().toString();
+        String phone = ((EditText) view.findViewById(R.id.editTextTeacherPhone)).getText().toString();
+        String cellphone = ((EditText) view.findViewById(R.id.editTextTeacherCellphone)).getText().toString();
+        String course = ((EditText) view.findViewById(R.id.editTextTeacherCourse)).getText().toString();
+
+
+
     }
 }
--- app/src/main/java/com/reforcointeligente/brainstormapp/View/LessonFormActivity.java
+++ app/src/main/java/com/reforcointeligente/brainstormapp/View/LessonFormActivity.java
@@ -1,5 +1,6 @@
 package com.reforcointeligente.brainstormapp.View;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,20 +8,35 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.reforcointeligente.brainstormapp.Model.Lesson;
+import com.reforcointeligente.brainstormapp.Model.Student;
 import com.reforcointeligente.brainstormapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LessonFormActivity extends AppCompatActivity {
 
     private Button confirmLessonButton;
     private Button cancelLessonButton;
     Spinner spinner;
+    private static DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson_form);
+
+        setUpStudentSpinner();
+        setUpSubjectSpinner();
 
         cancelLessonButton = (Button) findViewById(R.id.buttonCancelLesson);
         cancelLessonButton.setOnClickListener(new View.OnClickListener() {
@@ -39,8 +55,8 @@ public class LessonFormActivity extends AppCompatActivity {
         });
     }
 
-    private void setUpSpinner() {
-        spinner = (Spinner) findViewById(R.id.spinnerSubject);
+    private void setUpSubjectSpinner() {
+        spinner = (Spinner) findViewById(R.id.spinnerLessonSubject);
         ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this,
                 R.array.list_of_subjects, android.R.layout.simple_spinner_dropdown_item);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -48,35 +64,34 @@ public class LessonFormActivity extends AppCompatActivity {
         spinner.setAdapter(arrayAdapter);
     }
 
-    public void onLessonCreated() {
-//        Lesson lesson = new Lesson();
-//
-//        EditText dateEditText = (EditText) findViewById(R.id.editTextDate);
-//        EditText timeEditText = (EditText) findViewById(R.id.editTextTime);
-//        EditText placeEditText = (EditText) findViewById(R.id.editTextPlace);
-//        EditText displacementEditText = (EditText) findViewById(R.id.editTextDisplacement);
-//        EditText valuePerHourEditText = (EditText) findViewById(R.id.editTextValuePerHour);
-//        EditText durationLessonEditText = (EditText) findViewById(R.id.editTextLessonDuration);
-//        EditText totalLessonValueEditText = (EditText) findViewById(R.id.editTextTotalLessonValue);
-//        EditText valuePaidEditText = (EditText) findViewById(R.id.editTextValuePaid);
-//
-//        String subject = spinner.getSelectedItem().toString();
-//        String date = dateEditText.getText().toString();
-//        String time = timeEditText.getText().toString();
-//        String place = placeEditText.getText().toString();
-//        String displacement = displacementEditText.getText().toString();
-//        Double valuePerHour = Double.valueOf(valuePerHourEditText.getText().toString());
-//        Double duration = Double.valueOf(durationLessonEditText.getText().toString());
-//        Double totalLessonValue = Double.valueOf(totalLessonValueEditText.getText().toString());
-//        Double valuePaid = Double.valueOf(valuePaidEditText.getText().toString());
-//
-//        lesson.setLessonDate(date);
-//        lesson.setLessonTime(time);
-//        lesson.setLessonSubject(subject);
-//        lesson.setLessonPlace(place);
-//        lesson.setLessonDuration(duration);
+    private void setUpStudentSpinner() {
+        DatabaseReference reference = databaseReference.child("Students");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                final List<String> studentsList = new ArrayList<String>();
+                studentsList.add("");
 
-        // falta displacement, value per hour, total lesson value e value paid
+                for (DataSnapshot studentSnapshot: dataSnapshot.getChildren()) {
+                    Student student = studentSnapshot.child("Student").getValue(Student.class);
+                    studentsList.add(student.getStudentName());
+                }
+
+                Spinner studentSpinner = (Spinner) findViewById(R.id.spinnerLessonStudent);
+                ArrayAdapter<String> studentAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, studentsList);
+                studentAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                studentSpinner.setAdapter(studentAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    public void onLessonCreated() {
 
 
     }

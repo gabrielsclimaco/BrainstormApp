5b980c49e9f7325d5960142d04c421e1b7544461
climaco02 <gabrielsclimaco@gmail.com>
2017-01-16 21:19:05 -0200

    [Adding lesson Storie] Saving Lesson
    Removing unused variables and imports
    Student spinner sets student but does not show
    Calls method to save student on the click of the confirm button

--- app/src/main/java/com/reforcointeligente/brainstormapp/View/LessonFormActivity.java
+++ app/src/main/java/com/reforcointeligente/brainstormapp/View/LessonFormActivity.java
@@ -1,35 +1,23 @@
 package com.reforcointeligente.brainstormapp.View;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 
-import com.firebase.ui.database.FirebaseListAdapter;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.reforcointeligente.brainstormapp.Model.Lesson;
-import com.reforcointeligente.brainstormapp.Model.Student;
+import com.reforcointeligente.brainstormapp.Controller.FirebaseUtils;
 import com.reforcointeligente.brainstormapp.R;
+import com.reforcointeligente.brainstormapp.View.Adapter.StudentSpinnerAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LessonFormActivity extends AppCompatActivity {
-
-    private Button confirmLessonButton;
-    private Button cancelLessonButton;
-    Spinner spinner;
-    private static DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +26,7 @@ public class LessonFormActivity extends AppCompatActivity {
         setUpStudentSpinner();
         setUpSubjectSpinner();
 
-        cancelLessonButton = (Button) findViewById(R.id.buttonCancelLesson);
+        Button cancelLessonButton = (Button) findViewById(R.id.buttonCancelLesson);
         cancelLessonButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,53 +34,40 @@ public class LessonFormActivity extends AppCompatActivity {
             }
         });
 
-        confirmLessonButton = (Button) findViewById(R.id.buttonConfirmLesson);
+        Button confirmLessonButton = (Button) findViewById(R.id.buttonConfirmLesson);
         confirmLessonButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                onLessonCreated();
             }
         });
     }
 
     private void setUpSubjectSpinner() {
-        spinner = (Spinner) findViewById(R.id.spinnerLessonSubject);
-        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this,
+        ArrayAdapter<CharSequence> subjectAdapter = ArrayAdapter.createFromResource(this,
                 R.array.list_of_subjects, android.R.layout.simple_spinner_dropdown_item);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        subjectAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        spinner.setAdapter(arrayAdapter);
+        Spinner subjectSpinner = (Spinner) findViewById(R.id.spinnerLessonSubject);
+        subjectSpinner.setAdapter(subjectAdapter);
     }
 
     private void setUpStudentSpinner() {
-        DatabaseReference reference = databaseReference.child("Students");
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                final List<String> studentsList = new ArrayList<String>();
-                studentsList.add("");
+        ArrayList<String> studentsList = FirebaseUtils.getStudentsList();
 
-                for (DataSnapshot studentSnapshot: dataSnapshot.getChildren()) {
-                    Student student = studentSnapshot.child("Student").getValue(Student.class);
-                    studentsList.add(student.getStudentName());
-                }
+        ArrayAdapter<String> studentAdapter = new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_spinner_dropdown_item, studentsList);
+        studentAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-                Spinner studentSpinner = (Spinner) findViewById(R.id.spinnerLessonStudent);
-                ArrayAdapter<String> studentAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, studentsList);
-                studentAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                studentSpinner.setAdapter(studentAdapter);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
+        Spinner studentSpinner = (Spinner) findViewById(R.id.spinnerLessonStudent);
+        studentSpinner.setAdapter(studentAdapter);
+//        studentSpinner.setAdapter(new StudentSpinnerAdapter(this, new ArrayList<String>()));
     }
 
     public void onLessonCreated() {
+        View view = findViewById(R.id.activity_lesson_form);
+        FirebaseUtils.saveLesson(view);
 
-
+        finish();
     }
 }
